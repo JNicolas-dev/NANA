7aa6ae266b8b6c7dfa4ab777678b93907d5283c9
JNicolas-dev <nikolashernandez137@gmail.com>
2026-03-05 08:25:47 -0500

    hola 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const texto = "Siempre voy a estar aquí para ti, incluso cuando el mundo se sienta pesado.\n\n Te quiero mucho";
+const texto = "Siempre voy a estar aquí para ti, incluso cuando el mundo se sienta pesado.";
 const mensaje = document.getElementById("mensaje");
 mensaje.textContent = ""; // limpia cualquier texto previo
 let i = 0;
